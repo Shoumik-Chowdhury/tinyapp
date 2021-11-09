53487ee8ddac2684c412404aec1207671e9774dc
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-09 03:05:37 +0000

    add body-parser and dummy app.post response

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");  
 
 const urlDatabase = {
@@ -28,6 +31,11 @@ app.get("/urls", (req, res) => {
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
