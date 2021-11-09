1ff9fb4cbbb592353802b207c11b10fb42cb5406
Shoumik-Chowdhury <shoumik.atique@gmail.com>
2021-11-09 04:00:23 +0000

    add redirect to /urls/:shortURL after post request

--- express_server.js
+++ express_server.js
@@ -44,8 +44,9 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
